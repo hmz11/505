c039654c9015bad2133e068b5d3cad89aa9b6d98
hmz11 <294647990@qq.com>
2018-08-17 17:34:14 +0800

    重构

--- caculator/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ caculator/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -14,80 +14,74 @@ namespace WindowsFormsApp1
     public partial class Form1 : Form
     {
 
-        bool check = true; //判断电路类型
+        bool choice = true; //判断电路类型
         double R1 = 0;
         double R2 = 0;
         double R3 = 0;
         double R4 = 0;
         double vol = 0;
         double res = 0;                                                                 //结果
-        
+
 
         public Form1()
         {
             InitializeComponent();
-            MessageBox.Show("请输入电阻电压值并选择电路");
+            MessageBox.Show("欢迎，请输入电阻电压值并选择电路");
         }
-     
 
-        public void Result (bool check)                                                 //结果函数
+
+        public void Result(bool choice)                                                 //结果函数
         {
-            bool flag = true;                                                           //flag判断是否为无效输入
-
-            if(R1 <=0||R2<=0 || R3<=0 || R4 <= 0)
-            {
-                MessageBox.Show("输入无效，电阻不可为0或负数，请重新检查输入！");
-                flag = false;
-            }
-
-            if(vol <= 0)
-            {
-                MessageBox.Show("输入无效，电压不可为0或负数，请重新检查输入！");
-                flag = false;
-                
-            }
-                                                                                     //如果输入合法
-            if (check==true&&flag==true)
+                                                                                        //flag判断是否为无效输入
+            var flag = CheckInput();
+            if (flag == false)
             {
                 res = 0;
-                                                                                     //调用计算函数得出结果I
-                Cal result = new Cal();                                     
-                res = result.Calculate(vol, R1, R2, R3, R4, check);           
-            }
-
-            if (check == false && flag == true)
-            {
-                Cal result = new Cal();
-                res = result.Calculate(vol, R1, R2, R3, R4, check);
-            }
-
-            if(flag==false)
-            {
-                res = 0;                                                               //如果出错结果保持为0
-            }
+                return;                                                                 //如果出错结果保持为0
+            }                                                                                            
+            usecalculate(choice);                                                       //输入合法调用计算函数 
         }
-
-        private void button1_Click(object sender, EventArgs e)                         //选择电路1
+                                                                                        //调用计算函数
+        private void usecalculate(bool choice)
         {
-            check = true;
-            Result(check);
-            textBox5.Text = res.ToString("f3"); 
+            Cal result = new Cal();
+            res = result.Calculate(vol, R1, R2, R3, R4, choice);
+        }
+                                                                                        //判断输入是否有效
+        private bool CheckInput()
+        {
+            var flag = true;
+            if (R1 <= 0 || R2 <= 0 || R3 <= 0 || R4 <= 0|| vol <= 0)
+            {
+                MessageBox.Show("输入无效，电阻与电压不可为0，请重新检查输入！");
+                flag = false;
+            }
+            return flag;
         }
 
+        private void button1_Click(object sender, EventArgs e)                              //选择电路1
+        {
+            choice= true;
+            choose();
+        }
+                
         private void button2_Click(object sender, EventArgs e)                           //选择电路2
         {
-            
-            check = false;
-            Result(check);
-            textBox5.Text = res.ToString("f3");                                           //保留3位有效数字
+            choice = false;
+            choose();                                       
         }
+        private void choose()
+        {            
+                Result(choice);
+                textBox5.Text = res.ToString("f3");  //保留3位有效数字
+        }
+
 
 
         //************下为输入电阻和电压值以及键盘录入规则*************
-
-        private void textBox5_TextChanged(object sender, EventArgs e)
+         private void textBox5_TextChanged(object sender, EventArgs e)
         {
-         
+
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
@@ -97,7 +91,6 @@ namespace WindowsFormsApp1
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
             R2 = double.Parse(textBox2.Text);
         }
 
@@ -115,7 +108,63 @@ namespace WindowsFormsApp1
         {
             vol = double.Parse(textBox6.Text);
         }
+ 
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = this.textBox1.Text;
+            InputOk(e, str);
+        }
+    
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = this.textBox2.Text;
+            InputOk(e, str);
+        }
+        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = this.textBox3.Text;
+            InputOk(e, str);
+        }
+        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = this.textBox4.Text;
+            InputOk(e, str);
+        }
+        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            string str = this.textBox6.Text;
+            InputOk(e, str);
+        }
 
+        private static void InputOk(KeyPressEventArgs e, string str)
+        {
+            e.Handled = e.KeyChar < '0' || e.KeyChar > '9';                                     //允许输入数字
+            if (e.KeyChar == (char)8)                                                           //允许输入回退键
+            {
+                e.Handled = false;
+
+            }
+            if (e.KeyChar == (char)46)
+            {
+                if (str == "")                                                                  //第一个不允许输入小数点
+                {
+                    e.Handled = true;
+                    return;
+                }
+                else
+                {                                                                               //小数点不允许出现2次
+                    foreach (char ch in str)
+                    {
+                        if (char.IsPunctuation(ch))
+                        {
+                            e.Handled = true;
+                            return;
+                        }
+                    }
+                    e.Handled = false;
+                }
+            }
+        }
         private void label1_Click(object sender, EventArgs e)
         {
 
@@ -125,152 +174,5 @@ namespace WindowsFormsApp1
         {
 
         }
-
-        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            string str = this.textBox1.Text;
-            e.Handled = e.KeyChar < '0' || e.KeyChar > '9'; //允许输入数字
-            if (e.KeyChar == (char)8) //允许输入回退键
-            {
-                e.Handled = false;
-            }
-            if (e.KeyChar == (char)46)
-            {
-                if (str == "") //第一个不允许输入小数点
-                {
-                    e.Handled = true;
-                    return;
-                }
-                else
-                { //小数点不允许出现2次
-                    foreach (char ch in str)
-                    {
-                        if (char.IsPunctuation(ch))
-                        {
-                            e.Handled = true;
-                            return;
-                        }
-                    }
-                    e.Handled = false;
-                }
-            }
-        }
-        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            string str = this.textBox2.Text;
-            e.Handled = e.KeyChar < '0' || e.KeyChar > '9'; //允许输入数字
-            if (e.KeyChar == (char)8) //允许输入回退键
-            {
-                e.Handled = false;
-            }
-            if (e.KeyChar == (char)46)
-            {
-                if (str == "") //第一个不允许输入小数点
-                {
-                    e.Handled = true;
-                    return;
-                }
-                else
-                { //小数点不允许出现2次
-                    foreach (char ch in str)
-                    {
-                        if (char.IsPunctuation(ch))
-                        {
-                            e.Handled = true;
-                            return;
-                        }
-                    }
-                    e.Handled = false;
-                }
-            }
-        }
-        private void textBox3_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            string str = this.textBox3.Text;
-            e.Handled = e.KeyChar < '0' || e.KeyChar > '9'; //允许输入数字
-            if (e.KeyChar == (char)8) //允许输入回退键
-            {
-                e.Handled = false;
-            }
-            if (e.KeyChar == (char)46)
-            {
-                if (str == "") //第一个不允许输入小数点
-                {
-                    e.Handled = true;
-                    return;
-                }
-                else
-                { //小数点不允许出现2次
-                    foreach (char ch in str)
-                    {
-                        if (char.IsPunctuation(ch))
-                        {
-                            e.Handled = true;
-                            return;
-                        }
-                    }
-                    e.Handled = false;
-                }
-            }
-        }
-        private void textBox4_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            string str = this.textBox4.Text;
-            e.Handled = e.KeyChar < '0' || e.KeyChar > '9'; //允许输入数字
-            if (e.KeyChar == (char)8) //允许输入回退键
-            {
-                e.Handled = false;
-            }
-            if (e.KeyChar == (char)46)
-            {
-                if (str == "") //第一个不允许输入小数点
-                {
-                    e.Handled = true;
-                    return;
-                }
-                else
-                { //小数点不允许出现2次
-                    foreach (char ch in str)
-                    {
-                        if (char.IsPunctuation(ch))
-                        {
-                            e.Handled = true;
-                            return;
-                        }
-                    }
-                    e.Handled = false;
-                }
-            }
-        }
-        private void textBox6_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            string str = this.textBox6.Text;
-            e.Handled = e.KeyChar < '0' || e.KeyChar > '9'; //允许输入数字
-            if (e.KeyChar == (char)8) //允许输入回退键
-            {
-                e.Handled = false;
-            }
-            if (e.KeyChar == (char)46)
-            {
-                if (str == "") //第一个不允许输入小数点
-                {
-                    e.Handled = true;
-                    return;
-                }
-                else
-                { //小数点不允许出现2次
-                    foreach (char ch in str)
-                    {
-                        if (char.IsPunctuation(ch))
-                        {
-                            e.Handled = true;
-                            return;
-                        }
-                    }
-                    e.Handled = false;
-                }
-            }
-        }
-
     }
 }
